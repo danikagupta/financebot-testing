dbedf6daf33d701ae8ac64d72cb37cdad13a134a
Anoushka <139593644+anoushka-swami@users.noreply.github.com>
2023-10-25 19:38:19 +0000

    edited page descriptions and added message for def

--- pages/1_Definitions.py
+++ pages/1_Definitions.py
@@ -8,14 +8,16 @@ st.set_page_config(
 (column1,column2)=st.columns([3,7])
 column1.image("Personalized.png", width=100)
 column2.title("Definitions")
-st.subheader("Learn basic personal finance terms and gain a jumping-off point for the ask page")
+#st.subheader("Learn basic personal finance terms and gain a jumping-off point for the ask page")
 
-"""Savings:
-Investing:
-Credit:
-Debt:
-Loans:
-Interest:
-Budgeting:
-Types of income:
-"""
+"This page is currently under development---come back later!"
+
+#"""Savings:
+#Investing:
+#Credit:
+#Debt:
+#Loans:
+#Interest:
+#Budgeting:
+#Types of income:
+#"""
