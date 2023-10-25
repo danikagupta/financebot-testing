e10595e561bb140197b5f0df367518c9b2e91569
Anoushka <139593644+anoushka-swami@users.noreply.github.com>
2023-10-25 06:07:21 +0000

    updated About page

--- pages/1_about.py
+++ pages/1_about.py
@@ -12,9 +12,9 @@ st.write("""
          **:green[Welcome to Money Matters!]** Here, we hope to help you take charge of your financial destiny 
          by learning about personal finance. 
          
-         - With the **ask** page, you can ask our Chat-GPT-based AI any burning finance questions you have.
-         - With the **learn** page you can quiz yourself on personal finance basics.
-         - With the **analyze** page you can paste in any financial statement or company press release and we will summarize it and assess whether or not the company would be a good investment right now.
+         - With the **:green[ask]** page, you can ask our Chat-GPT-based AI any burning finance questions you have.
+         - With the **:green[learn]** page, you can quiz yourself on personal finance basics.
+         - With the **:green[analyze]** page, you can paste in any financial statement or company press release and we will summarize it and assess whether or not the company would be a good investment right now.
          
          We have augmented our AI using resources from Khan Academy, the Federal Reserve Bank of St. Louis, TED-Ed, DardenMBA, PBS, and moneyinstructor.com, and *greatly appreciate* being able to build on their great work.
          """)
