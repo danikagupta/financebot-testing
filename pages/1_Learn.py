6a83f7133f8ba192856c527582ab347728d8aca0
Anoushka <139593644+anoushka-swami@users.noreply.github.com>
2023-10-26 00:08:49 +0000

    renaming and adding defs

---
+++ pages/1_Learn.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Definitions | Money Matters",
+    page_icon="Personalized.png",
+)
+
+(column1,column2)=st.columns([3,7])
+column1.image("Personalized.png", width=100)
+column2.title("Definitions")
+#st.subheader("Learn basic personal finance terms and gain a jumping-off point for the ask page")
+
+st.write("""Savings: Money set away, perhaps to compound over time, in order to be retrieved later. Some examples of usage include retirement savings, an emergency fund in case of a sudden financial need, and saving up for a big purchase.
+Investing:To put money into an asset (such as stocks, bonds, real estate, or gold) with the expectation that it will grow in the future.
+Credit: The ability to make a purchase and pay for it later.
+Debt: Money which is owed to someone.
+Loans: Money which is given, typically by a bank, with the expectation of later repayment.
+Interest: Additional money which you pay to borrow money.
+Budgeting: Creating a plan to allot money.
+""")
